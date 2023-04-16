3e70a5890e3b758db52d55c11e22c99dcaef70c7
Evan ☕ <14357936+cl1ents@users.noreply.github.com>
2023-04-16 03:16:50 +0200

    Added crouch, crouch jump and various tweaks

--- Player.py
+++ Player.py
@@ -20,6 +20,23 @@ class Player(PhysicsObject):
     mask = pymunk.ShapeFilter(mask=pymunk.ShapeFilter.ALL_MASKS()^PLAYER_CATEGORY)
     maxAngularVelocity = math.pi*5
 
+    stabilisationForce = 3500000
+    stabilisationDampening = Vec2d(400, 2000)
+
+    leftRayOffset = Vec2d(-35,0)
+    rightRayOffset = Vec2d(35,0)
+    
+    rayDistance = 85
+
+    rayAlpha = 1-50/rayDistance
+    rayAlphaCrouch = 1-25/rayDistance
+
+    jumpForce = 40000000
+    crouchJumpMultiplier = 1.1
+
+    jumpingCooldown = .1
+    airControlCooldown = .02
+
     def __init__(self, app):
         super().__init__(app)
         
@@ -35,10 +52,15 @@ class Player(PhysicsObject):
         self.hoverboard.filter = pymunk.ShapeFilter(categories = PLAYER_CATEGORY)
         
         # Input
-        self.jumpingCooldown = .1
-        self.jumping = self.jumpingCooldown
+        self.jumpTick = self.jumpingCooldown
         self.jumpKey = False
         self.moveVector = Vector2(0,0)
+
+        self.gliding = False
+
+        self.crouch = False
+
+        self.airControlTick = 0
         
         # Render
         self.image = pygame.Surface((100, 10), pygame.SRCALPHA)
@@ -62,6 +84,10 @@ class Player(PhysicsObject):
                             self.moveVector.x -= 1
                         case pygame.K_d:
                             self.moveVector.x += 1
+                        case pygame.K_s:
+                            self.crouch = True
+                        case pygame.K_a:
+                            self.gliding = True
                 case pygame.KEYUP:
                     match event.key:
                         case pygame.K_SPACE:
@@ -70,6 +96,10 @@ class Player(PhysicsObject):
                             self.moveVector.x += 1
                         case pygame.K_d:
                             self.moveVector.x -= 1
+                        case pygame.K_s:
+                            self.crouch = False
+                        case pygame.K_a:
+                            self.gliding = False
 
     def debug_event(self, event):
         match event.type:
@@ -84,28 +114,58 @@ class Player(PhysicsObject):
 
     def jump(self, state):
         self.jumpKey = state
-        if self.jumping > self.jumpingCooldown and state:
-            self.jumping = 0
+        if self.jumpTick > self.jumpingCooldown and state:
+            self.jumpTick = 0
 
     def update(self):
         app = self.app
 
+        distanceMultiplication = self.rayAlphaCrouch if self.crouch else self.rayAlpha
+        jumpMultiplier = self.crouchJumpMultiplier if self.crouch else 1
+        
+        # leftDip = self.moveVector.x > 0
+
         # Left ray
-        left = self.hoverRay(Vec2d(-35,0), 100-(abs(clamp(self.moveVector.x, -1, 0)))*10)# getPointAtAngle(self.body.position, -self.body.angle-math.pi/2, 30))
+        left = self.hoverRay(self.leftRayOffset, self.rayDistance)
         # Right ray
-        right = self.hoverRay(Vec2d(35,0), 100-(abs(clamp(self.moveVector.x, 0, 1)))*10)
+        right = self.hoverRay(self.rightRayOffset, self.rayDistance)
     
         floor = left or right
-        if self.jumping == 0 and floor:
-            self.body.apply_impulse_at_local_point((0,500000))
-        if floor and self.jumping > self.jumpingCooldown: # Stick to floor
-            self.body.apply_impulse_at_local_point((self.moveVector.x*1600000*app.deltaTime, 0))
+        if floor:
+            self.airControlTick = 0
         else:
+            self.airControlTick += app.deltaTime
+
+
+        if self.jumpTick == 0 and floor:
+            # self.body.apply_impulse_at_local_point((0,500000))
+            if left:
+                localNormal = self.body.world_to_local(self.body.position+left.normal)
+                self.body.apply_force_at_local_point(localNormal*self.jumpForce*jumpMultiplier, (-35,0))
+            if right:
+                localNormal = self.body.world_to_local(self.body.position+right.normal)
+                self.body.apply_force_at_local_point(localNormal*self.jumpForce*jumpMultiplier, (35,0))
+        elif floor and self.jumpTick > self.jumpingCooldown: # Stick to floor
+            if left:
+                dip = 1 + (self.moveVector.x < 0) * .075
+
+                origin = self.body.local_to_world(self.leftRayOffset)
+                currentVel = self.body.world_to_local(origin+self.body.velocity_at_local_point(self.leftRayOffset))
+                self.body.apply_force_at_local_point(Vec2d(0, (((2-2*left.alpha)-(2*dip*distanceMultiplication))*self.stabilisationForce))-Vec2d(currentVel.x*self.stabilisationDampening.x, currentVel.y*self.stabilisationDampening.y), self.leftRayOffset)
+            if right:
+                dip = 1 + (self.moveVector.x > 0) * .075
+
+                origin = self.body.local_to_world(self.rightRayOffset)
+                currentVel = self.body.world_to_local(origin+self.body.velocity_at_local_point(self.rightRayOffset))
+                self.body.apply_force_at_local_point(Vec2d(0, (((2-2*right.alpha)-(2*dip*distanceMultiplication))*self.stabilisationForce))-Vec2d(currentVel.x*self.stabilisationDampening.x, currentVel.y*self.stabilisationDampening.y), self.rightRayOffset)
+                
+            self.body.apply_force_at_local_point((self.moveVector.x*1600000, 0))
+        elif self.airControlTick > self.airControlCooldown:
             f = max if -self.moveVector.x > 0 else min
             if abs(f(self.body.angular_velocity, 0)) < self.maxAngularVelocity:
                 self.body.angular_velocity += min(-self.moveVector.x*app.deltaTime*50, self.maxAngularVelocity-self.body.angular_velocity)
 
-        self.jumping += app.deltaTime
+        self.jumpTick += app.deltaTime
         super().update()
 
     def hoverRay(self, offset, dist=100):
@@ -116,10 +176,6 @@ class Player(PhysicsObject):
 
         seg = app.space.segment_query_first(origin, target, 2, self.mask)
 
-        if seg:
-            currentVel = self.body.world_to_local(origin+self.body.velocity_at_local_point(offset))
-            # print(2/app.deltaTime)
-            self.body.apply_impulse_at_local_point(Vec2d(0, (((2-2*seg.alpha)-1)*1500000)*app.deltaTime)-currentVel*400*app.deltaTime, offset)
         return seg
 
     def render(self):
