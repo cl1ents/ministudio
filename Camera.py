06c2c2aae87503b50ab1fb910f9e0fa73454fb85
evn <gh@cli.lol>
2023-04-27 12:36:05 +0200

    zzz

--- Camera.py
+++ Camera.py
@@ -13,7 +13,7 @@ from easing_functions import *
 class Camera:
     mask = pymunk.ShapeFilter(mask=pymunk.ShapeFilter.ALL_MASKS()^PLAYER_CATEGORY^ENEMY_CATEGORY)
     minFov = 0.9
-    maxFov = 1.4
+    maxFov = 1.3
     maxSpeed = 2500
     scrollSpeed = 600
 
@@ -32,14 +32,16 @@ class Camera:
         app = self.app
         player = app.Player
 
-        self.xScrollPos = max(self.xScrollPos + self.scrollSpeed * self.app.deltaTime, player.body.position.x)
+
+
+        self.xScrollPos = max(self.xScrollPos + (self.scrollSpeed * app.gaming) * self.app.deltaTime, player.body.position.x)
 
         scrollPos = Vec2d(self.xScrollPos, player.body.position.y)
         seg = app.space.segment_query_first(scrollPos, scrollPos+Vec2d(0, -500), 2, self.mask)
 
         position = seg.point if seg else scrollPos+Vec2d(0, -500)
         self.goal = Vec2d(position.x, position.y+app.screenSize.y/4)
-        self.goal += Vec2d(player.body.velocity.x * .2, player.body.velocity.y * .1)
+        self.goal += Vec2d(player.body.velocity.x * .2, player.body.velocity.y * .01)
         self.position = Vec2d(self.position.x+(self.goal.x-self.position.x)*min(self.app.deltaTime*15, 1), self.position.y+(self.goal.y-self.position.y)*min(self.app.deltaTime*10, 1))
         self.fovGoal = self.CalculateFOV(int(player.body.velocity.get_distance((0,0))))
         
